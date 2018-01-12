80a11cafd5b45deaa90ac55ad639b827b33ffda0
Your Name <Your Email>
2018-01-12 16:29:08 +0000

    cp Makefile.in_dong Makefile.in and sudo apt-get install autoconf automake libtool

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 
+#sudo apt-get install autoconf automake libtool
+
 ./autogen.sh 
+
+cp Makefile.in_dong Makefile.in
 
 ./configure --enable-bitmain-D1 --without-curses --host=arm-xilinx-linux-gnueabi --build=x86_64-pc-linux-gnu
